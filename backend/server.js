944001b60ed99454be70bf02ec74e83329bbb4a2
Petruska Tomi <tamas.petruska99@gmail.com>
2022-11-08 11:14:01 +0100

    menu.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const app = express();
 const path = require("path");
 
+app.use(express.json());
+
 app.listen(9000, (_) => console.log("127.0.0.1:9000"));
 
 app.use("/public", express.static(`${__dirname}/../frontend/public`));
@@ -11,4 +13,41 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
-app.post("/order-pizza", (req, res) => {});
+app.get("/menu", (req, res) => {
+  fs.readFile(`${__dirname}/data/menu.json`, function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    } else {
+      studentData = JSON.parse(data);
+      return res.send(studentData);
+    }
+  });
+});
+
+app.post("/order-pizza", (req, res) => {
+  console.log(req.body);
+
+  const fileData = req.body;
+  const uploadPath = path.join(`${__dirname}/data/orders.json`);
+
+  console.log(fileData);
+
+  fs.readFile(`${__dirname}/data/orders.json`, function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    } else {
+      const orders = JSON.parse(data);
+      orders.push(fileData);
+      fs.writeFile(uploadPath, JSON.stringify(orders, null, 2), (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        } else {
+          return res.send("ORDER SENT!");
+        }
+      });
+    }
+  });
+});
