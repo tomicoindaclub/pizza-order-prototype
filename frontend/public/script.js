c37da4f2b800f68dfedbc96f557b00f596617b32
Petruska Tomi <tamas.petruska99@gmail.com>
2022-11-09 19:00:33 +0100

    script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -14,12 +14,12 @@ const menuComponent = function (id, pic, pizzaName, ingredients) {
         <input
             class="quantity"
             type="number"
-            id="${id}-input"
+            id="input${id}"
             name="quantity"
             min="0"
             max="10"
         />
-        <input class="kosar-button" type="button" value="kosárba" />
+        <input id="btn${id}" class="kosar-button" type="button" value="kosárba" />
     </form>
 </div>`;
 };
@@ -32,7 +32,9 @@ const orderComponent = function (id, pic, pizzaName, ingredients, amount) {
         <h2>${pizzaName}</h2>
             <p>${ingredients}</p>
     </span>
-    <h2>x${amount}</h2>
+    <form>
+        <h2>x${amount}</h2>
+    </form>
 </div>`;
 };
 
@@ -42,26 +44,46 @@ const fetchMenu = async () => {
 
 async function loadEvent() {
   const menu = await fetchMenu();
-  console.log(menu);
 
-  const menuDataList = menu;
-
-  for (let i = 0; i < menuDataList.length; i++) {
+  for (let i = 0; i < menu.length; i++) {
     menuList.insertAdjacentHTML(
       "beforeend",
       menuComponent(
-        menuDataList[i].id,
-        menuDataList[i].pic,
-        menuDataList[i].pizzaName,
-        menuDataList[i].ingredients
+        menu[i].id,
+        menu[i].pic,
+        menu[i].pizzaName,
+        menu[i].ingredients
       )
     );
+  }
+
+  const buttons = document.querySelectorAll(".kosar-button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", async function () {
+      let itemID = parseInt(this.id.substring(3));
+      let orderAmount = document.querySelector("#input" + itemID).value;
+
+      const menu = await fetchMenu();
+      console.log(menu);
+      console.log(itemID);
+      console.log(orderAmount);
+
+      for (let i = 0; i < menu.length; i++) {
+        if (itemID === menu[i].id) {
+          orderList.insertAdjacentHTML(
+            "beforeend",
+            orderComponent(
+              menu[i].id,
+              menu[i].pic,
+              menu[i].pizzaName,
+              menu[i].ingredients,
+              orderAmount
+            )
+          );
+        }
+      }
+    });
   }
 }
 
 rootElement.addEventListener("loaded", loadEvent());
-
-const addCartButton = document.querySelector(".kosar-button");
-addCartButton.addEventListener("click", function () {
-  const orderAmount = document.querySelector();
-});
