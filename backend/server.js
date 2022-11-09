202dfa276fdfa6b0fa950ddcd4f15be5c9810120
Petruska Tomi <tamas.petruska99@gmail.com>
2022-11-09 09:22:39 +0100

    backend.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.listen(9000, (_) => console.log("127.0.0.1:9000"));
 
 app.use("/public", express.static(`${__dirname}/../frontend/public`));
 
+app.use("/public", express.static(`${__dirname}/backend/data/img`));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
