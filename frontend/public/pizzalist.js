18bd7713239fa2965e79116939db490754c1c4ce
Petruska Tomi <tamas.petruska99@gmail.com>
2022-11-23 16:31:51 +0100

    csapatmunka

--- frontend/public/pizzalist.js
+++ frontend/public/pizzalist.js
@@ -18,17 +18,37 @@ const activeMenuComponent = function (id, pic, pizzaName, ingredients) {
 
 const inactiveMenuComponent = function (id, pic, pizzaName, ingredients) {
   return `
-<div class="pizza-card" id="${id}">
-    <p>ID: ${id}</p>
-    <img src="${pic}" alt="" />
-    <span>
-        <h2>${pizzaName}</h2>
-            <p>${ingredients}</p>
-    </span>
-    <button class="edit-button" id="btn${id}">Szerkesztés</button>
-    <input type="checkbox" id="chcek-${id}" name="${pizzaName}" />
-    <label for="${pizzaName}">Aktív?</label>
-</div>`;
+  <div class="pizza-card" id="${id}">
+      <p>ID: ${id}</p>
+      <img src="${pic}" alt="" />
+      <span>
+          <h2>${pizzaName}</h2>
+              <p>${ingredients}</p>
+      </span>
+      <button class="edit-button" id="btn${id}">Szerkesztés</button>
+      <input type="checkbox" id="chcek-${id}" name="${pizzaName}" />
+      <label for="${pizzaName}">Aktív?</label>
+  </div>`;
+};
+
+const editedMenuComponent = function (id, pizzaName, ingredients) {
+  return `
+  <div class="pizza-card" id="${id}">
+     <div class="form">
+          <form action="">
+            <input type="text" name="id"  value="${id}"/>
+            <input type="text" name="pizzaName"  value="${pizzaName}"/>
+            <input type="text" name="ingredients"  value="${ingredients}"/>
+            <label class="upload-button">
+              <input type="file" />
+              Kép feltöltése
+            </label>
+            <input type="file" name="pic" />
+          </form>
+      <button class="delete-button" id="delete${id}">delete</button>
+      <input type="checkbox" id="chcek-${id}" name="${pizzaName}" />
+      <label for="${pizzaName}">Aktív?</label>
+  </div>`;
 };
 
 const fetchMenu = async () => {
@@ -66,7 +86,7 @@ async function loadEvent() {
 
   const buttons = document.querySelectorAll(".edit-button");
   for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
+    buttons[i].addEventListener("click", async function () {
       // gombokkal valahogy le kell kezelni hogy az adott pizzának ami a json-ből lekért eredmény (itt egy menu nevű tömb) hányadik eleme, majd annak az elemnek a kulcsérték-ét módosítani kell, pl gombnyomásra megváltozik annak az elemnek a HTML-je és input mezők lesznek ahová új értékeket tudunk írni és ezután visszaküldeni az új értékeket
 
       let itemID = parseInt(this.id.substring(3));
@@ -74,7 +94,22 @@ async function loadEvent() {
       itemCards.forEach((selected) => {
         if (parseInt(selected.id) === itemID) {
           selectedItemCard = selected;
-          console.log(selectedItemCard);
+        }
+      });
+
+      menuItems = await fetchMenu();
+
+      console.log(selectedItemCard);
+
+      menuItems.forEach((editedItem) => {
+        if (parseInt(editedItem.id) === itemID) {
+          selectedItemCard.innerHTML(
+            editedMenuComponent(
+              editedItem.id,
+              editedItem.pizzaName,
+              editedItem.ingredients
+            )
+          );
         }
       });
     });
